11dda755d1a4f084623b901422062165abe233e6
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-10-06 12:06:00 +0530

    Update main.py

--- Python/src/main.py
+++ Python/src/main.py
@@ -1,7 +1,17 @@
+# Import the Bureau and the 'agent' from the respective modules
 from uagents import Bureau
 from agents.temperature import agent
 
-bureau = Bureau(agent)
+# Create a Bureau instance with the specified endpoint (you may want to replace 'h' with the actual endpoint)
+bureau = Bureau(endpoint='h')
 
+# Check if the script is the main program
 if __name__ == "__main__":
+    # Print a message indicating that the agent is being added to the Bureau
+    print(f"Adding agent to Bureau: {agent.address}")
+
+    # Add the 'agent' to the Bureau
+    bureau.add(agent)
+
+    # Run the Bureau's event loop
     bureau.run()
