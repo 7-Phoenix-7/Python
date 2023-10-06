a3a87d58b5e3d70fa15e1a9a257a4b8118907d7c
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-10-06 12:07:39 +0530

    Update message.py

--- Python/src/messages/message.py
+++ Python/src/messages/message.py
@@ -1,15 +1,20 @@
+# Import the Model class from the uagents module
 from uagents import Model
 
+# Define a custom model class 'TemperatureRequest' that inherits from Model
 class TemperatureRequest(Model):
-    location: str
-    min_temperature: float
-    max_temperature: float
+    # Define fields for the model class
+    location: str            # Represents the location for which temperature is requested
+    min_temperature: float   # Represents the minimum preferred temperature
+    max_temperature: float   # Represents the maximum preferred temperature
 
-
+# Define another custom model class 'TemperatureResponse' that inherits from Model
 class TemperatureResponse(Model):
-    location: str
-    current_temperature: float
+    # Define fields for the model class
+    location: str            # Represents the location for which temperature is responded
+    current_temperature: float   # Represents the current temperature at the specified location
 
-
+# Define another custom model class 'Error' that inherits from Model
 class Error(Model):
-    error: str
+    # Define fields for the model class
+    error: str               # Represents an error message
