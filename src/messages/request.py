b2a7f7d479cd8f8bf87c7adc471ec34e288fef2b
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-10-08 19:48:38 +0530

    Update request.py

--- src/messages/request.py
+++ src/messages/request.py
@@ -1,4 +1,7 @@
+# Import the Model class from the uagents library
 from uagents import Model
 
+# Define a message model named Request
 class Request(Model):
+    # Define an attribute 'response_address' to store the response address for the request
     response_address: str
