dd435a99bf7ea391476c9d31b639190ec95b9098
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-10-08 19:49:15 +0530

    Update userpreference_msg.py

--- src/messages/userpreference_msg.py
+++ src/messages/userpreference_msg.py
@@ -1,6 +1,12 @@
+# Import the Model class from the uagents library
 from uagents import Model
 
+# Define a message model named UserPreference
 class UserPreference(Model):
+    # Define three attributes:
+    # - 'minTemp' to store the preferred minimum temperature as a float
+    # - 'maxTemp' to store the preferred maximum temperature as a float
+    # - 'location' to store the preferred location as a string
     minTemp: float
     maxTemp: float
     location: str
