d07d23af383a483abf92e8ccb709a536525c116a
BhavikPawar29 <144596999+BhavikPawar29@users.noreply.github.com>
2023-10-08 19:48:01 +0530

    Update alert_msg.py

--- src/messages/alert_msg.py
+++ src/messages/alert_msg.py
@@ -1,5 +1,7 @@
+# Import the Model class from the uagents library
 from uagents import Model
 
+# Define a message model named Alert
 class Alert(Model):
+    # Define an attribute 'text' to store the text of the alert message
     text: str
-    
